ded2ad57f73d0d184e6f8edc15453ead7dbdb72f
Roy <wave7410@gmail.com>
2023-12-03 16:29:52 +0800

    update

--- src/views/Practice/Interface.vue
+++ src/views/Practice/Interface.vue
@@ -4,55 +4,70 @@ export default{
         return{
             show:false,
             description:"",  
-            amount:"",
-            money:[],
-            num:"",
-            sum:"",
+            amount:"",  
+            records:[],  //陣列儲存
+            type:'income', //預設為收入
         }
     },
+    computed:{
+        totalIncome() {
+            return this.records
+                .filter((record) => record.type === "income")
+                .reduce((total, record) => total + record.amount, 0);
+    },
+        totalExpense() {
+            return this.records
+                .filter((record) => record.type === "expense")
+                .reduce((total, record) => total + record.amount, 0);
+    },
+},
     methods:{
         submit(){
-            const form = document.querySelectorAll('input')
-            this.num = 0
-            form.forEach(item =>{
-                if(item.value.length == 0){
-                    alert("請填寫完整資訊")
+                //防呆
+                if (this.description && this.amount) {
+                    const record = {
+                        type: this.type,
+                        description: this.description,
+                        amount: parseFloat(this.amount)
+                    };
+                    this.records.push(record);
+                    this.saveToLocalStorage(); // 在新增記錄後保存到本地儲存空間
+                    this.close();
+                } else {
+                    alert("請填寫完整的資訊");
                 }
-                this.num++
-            })
-            if(this.num == form.length){
-                this.money.push({})
+        },
+        deleteRecord(index) {
+            const sureDelete = window.confirm("確定要刪除嗎？");
+
+            if (sureDelete) {
+                this.records.splice(index, 1);
+                this.saveToLocalStorage(); // 在刪除記錄後保存到本地儲存空間
             }
-            // // 防呆
-            // if(this.expend && this.price){
-            //     // 儲存置本機儲存空間
-            //     const data = {
-            //         expend: this.expend,
-            //         price: this.price
-            //     };
-            //     this.info.push(data);
-            //     localStorage.setItem("itemData", JSON.stringify(this.info));
-            //     this.close();
-            // }else{
-            //     alert("請填寫完整資訊")
-            // }
         },
         close(){
             this.show = false;
             // 重置欄位
             this.description = "";
             this.amount = "";
+            this.type = "income";
         },
-        delete(){
-            this.money.splice(index,1)
+        // 保存到本地儲存空間
+        saveToLocalStorage() {
+            localStorage.setItem("records", JSON.stringify(this.records));
+        },
+        // 在 mounted 階段從本地儲存取得資料
+        loadFromLocalStorage() {
+            const saved = JSON.parse(localStorage.getItem("records")) || [];
+            this.records = saved;
         }
     },
     mounted(){
-        const total = document.getElementById('total')
-        const income = document.getElementById('income')
-        const expend = document.getElementById('expend')
-        
-        let minus = []
+        this.loadFromLocalStorage();
+
+        this.records.forEach(record => {
+        console.log('Record Type:', record.type);
+         });
     }
 }
 </script>
@@ -62,32 +77,44 @@ export default{
     <div class="left">
         <h1>Expense Tracker</h1>
         <h2>Your Balance</h2>
-        <p id="total">$ 總金額</p>
+        <p id="total">$ {{totalIncome - totalExpense}}</p>
     </div>
     <div class="right">
         <div class="earn">
             <p>INCOME</p>
-            <p id="income">$ 摳摳</p>
+            <p id="income">$ {{ totalIncome }}</p>
         </div>
         <div class="purchase">
             <p>EXPENSE</p>
-            <p id="expend">$ 摳摳</p>
+            <p id="expense">$ {{ totalExpense }}</p>
         </div>
         <button type="button" @click="show = !show" class="add">Add Transaction</button>
         <div v-if="show" class="model">
             <!-- 彈出視窗內容 -->
             <label for="">項目 :</label>
-            <input type="text" v-model="expend" placeholder="請輸入項目">
+            <input type="text" v-model="description" id="description" placeholder="請輸入項目">
+
             <label for="">金額 :</label>
-            <input type="text" v-model="price" placeholder="請輸入金額">
+            <input type="text" v-model="amount" id="amount" placeholder="請輸入金額">
+
+            <div class="option">
+                <label for="">類型</label>
+                <select v-model="type" id="type">
+                <option value="income">收入</option>
+                <option value="expense">支出</option>
+                </select>
+            </div>
             <button @click="submit" class="check">確定</button>
             <button @click="close" class="check">關閉</button>
         </div>
         <div class="showArea">
         <ul>
-            <li v-for="(item,index) in info">
-                <span>項目:{{item.expend}}  金額:{{item.price}}</span>
-                <button type="button" @click="delete">Delete</button>
+            <li v-for="(item,index) in records" :key="index"
+            :style="{ borderRight: item.type === 'income' ? '5px solid green' : '5px solid red' }">
+            <button type="button" @click="deleteRecord(index)" class="delete-btn">
+                <i class="fa-sharp fa-solid fa-xmark"></i>
+            </button>
+                {{item.description}} <span class="details">{{item.amount}}</span>
             </li>
         </ul>
         </div>
@@ -178,6 +205,11 @@ export default{
                 margin-top: 50px;
                 border-radius: 5px;
             }
+            .option{
+                display: flex;
+                justify-content: center;
+                margin-top: 20px;
+            }
             .check{
                 margin: 30px 20px;
                 background-color: #51578f;
@@ -187,14 +219,47 @@ export default{
         }
         .showArea{
             position: absolute;
-            top: 70%;
-            left: 40%;
+            top: 60%;
+            left: 30%;
+            width: 350px;
+            height: 300px;
             overflow: auto;
+
             li{
                 list-style: none;
-                // width: 30%;
-                height: 30%;
-                border: 1px solid black;
+                display: flex;
+                justify-content: space-between;
+                margin-top: 10px;
+                font-size: 20pt;
+                border-radius: 5px;
+                background-color: #e2e8f0;
+                box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+                
+                .details{
+                    margin-left: 10px;
+                    display: inline-block;
+                }
+                .delete-btn{
+                    opacity: 0;
+                    background: none;
+                    border: none;
+                    cursor: pointer;
+                    font-size: 20pt;
+                    // margin-top:10px ;
+                    transition: opacity 0.3s ease;
+                    // position: absolute;
+                    // top: 0;
+                    // left: 0;
+                }
+                .delete-btn:hover{
+                    content: "";
+                    opacity: 1;
+                    color: red;
+                }
+                .fa-sharp::before {
+                    content: "\f00d";
+                    font-family: "Font Awesome 5 Free";
+                }
             }
         }
     }
